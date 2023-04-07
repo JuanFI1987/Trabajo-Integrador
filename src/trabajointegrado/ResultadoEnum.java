d066a7e92e588e5b0ae90e0051fedb0b7d9e9a4d
JuanFI1987 <ciaojuan@hotmail.com>
2023-04-07 11:23:37 -0300

    Commit

--- src/trabajointegrado/ResultadoEnum.java
+++ src/trabajointegrado/ResultadoEnum.java
@@ -3,5 +3,15 @@ package trabajointegrado;
 
 
 public class ResultadoEnum {
-     
+     String resultado;
+        if (partidoComoArreglo[2]>partidoComoArreglo[3]){
+            resultado = 'Ganador1';
+        }
+        else if(partidoComoArreglo[2]<partidoComoArreglo[3]){
+            resultado = 'Ganador2';
+        }
+        else {
+            resultado = 'Empate';
+        }
+    }
 }
