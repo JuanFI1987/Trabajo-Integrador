13fb001f17b48fefc4e3f26365bbdd510e9cd29a
JuanFI1987 <ciaojuan@hotmail.com>
2023-04-08 13:05:39 -0300

    Update ResultadoEnum.java

--- src/trabajointegrado/ResultadoEnum.java
+++ src/trabajointegrado/ResultadoEnum.java
@@ -1,17 +1,36 @@
 
 package trabajointegrado;
 
+import java.util.ArrayList;
+import static trabajointegrado.lecturaArchivosCSV.obtener;
+
 
 public class ResultadoEnum {
-     String resultado;
-        if (partidoComoArreglo[2]>partidoComoArreglo[3]){
-            resultado = 'Ganador1';
+     String resultado1;
+     ArrayList<Partido> partidos = obtener();
+     Partido partido1 = partidos.get(0);
+        if (partido1.getGolesEquipo1()>partido1.getGolesEquipo2()){
+            resultado1 = 'Ganador1';
         }
-        else if(partidoComoArreglo[2]<partidoComoArreglo[3]){
-            resultado = 'Ganador2';
+        else if(partido1.getGolesEquipo1()<partido1.getGolesEquipo2()){
+            resultado1 = 'Ganador2';
         }
         else {
-            resultado = 'Empate';
+            resultado1 = 'Empate';
         }
+    String resultado2;
+     ArrayList<Partido> partidos = obtener();
+     Partido partido2 = partidos.get(0);
+        if (partido2.getGolesEquipo1()>partido2.getGolesEquipo2()){
+            resultado2 = 'Ganador1';
+        }
+        else if(partido1.getGolesEquipo1()<partido1.getGolesEquipo2()){
+            resultado2 = 'Ganador2';
+        }
+        else {
+            resultado2 = 'Empate';
+        }
+    System.out.println(resultado1);
+    System.out.println(resultado2);
     }
 }
