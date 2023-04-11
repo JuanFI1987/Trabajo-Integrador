4c1ac88b2c3599cecc6346f8170cc118f6c35c55
JuanFI1987 <ciaojuan@hotmail.com>
2023-04-10 22:19:36 -0300

    Commit

--- src/trabajointegrado/TrabajoIntegrado.java
+++ src/trabajointegrado/TrabajoIntegrado.java
@@ -12,31 +12,22 @@ public class TrabajoIntegrado {
 
     public static void main(String[] args) {
         
-               
-        System.out.println("Tu puntaje total es de: " + Pronostico.calcularPuntajePronostico());
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        /*
+        System.out.println("El resultado de los partidos fue: ");       
+                
         ArrayList<Partido> partidos = obtener();
         for(int x=0; x < partidos.size(); x++){
             Partido partido = partidos.get(x);
-            System.out.println(partido.getEquipo1() + partido.getGolesEquipo1() + " vs " + partido.getGolesEquipo2()+ partido.getEquipo2());
+            System.out.println(partido.getEquipo1() + " " + partido.getGolesEquipo1() + " vs " + partido.getGolesEquipo2()+ " " + partido.getEquipo2());
         }
-        
+        /*
         ArrayList<Pronostico> pronosticos = obtenerPronostico();
         for(int x=0; x < pronosticos.size(); x++){
             Pronostico pronostico = pronosticos.get(x);
             System.out.println(pronostico.getGanador1() + " - " + pronostico.getGanador2() + " - " + pronostico.getEmpate());
         }
         */
+        
+        System.out.println("Tu puntaje total es de: " + Pronostico.calcularPuntajePronostico());
     }
     
 }
